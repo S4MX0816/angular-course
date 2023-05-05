28a97944f0815893ebea807fe6c09b257fabd853
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-06 01:25:17 +0530

    primitive types

---
+++ typescript-intro/basics.ts
@@ -0,0 +1,15 @@
+// TYPES
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number;
+age = 12;
+
+let username: string;
+username = "Sam";
+
+let isInstructor: boolean;
+isInstructor = true;
