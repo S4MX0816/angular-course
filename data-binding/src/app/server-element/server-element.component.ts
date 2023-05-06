864571c690d0d4e10e127df38caf5172958a9f4a
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-06 17:20:35 +0530

    view encapsulation

--- data-binding/src/app/server-element/server-element.component.ts
+++ data-binding/src/app/server-element/server-element.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { ServerElement } from './server-element.model';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class ServerElementComponent {
   @Input('srvElement') element: ServerElement = {} as ServerElement;
