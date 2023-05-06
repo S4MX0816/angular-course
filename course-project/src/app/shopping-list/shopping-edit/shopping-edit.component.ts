2d4234cedef75b29562a467571aa59537fa9ea95
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-07 00:51:24 +0530

    passing data between shopping components

--- course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent {
+  @ViewChild('amountInput', { static: true })
+  amountInput: ElementRef<HTMLInputElement>;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
+  onAddItem(nameInput: HTMLInputElement) {
+    this.ingredientAdded.emit(
+      new Ingredient(nameInput.value, +this.amountInput.nativeElement.value)
+    );
+  }
 }
