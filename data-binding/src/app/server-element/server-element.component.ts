96196dc86dadcb09afdb471cdded1f3c73d934b0
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-06 16:51:24 +0530

    property binding with input decorator

--- data-binding/src/app/server-element/server-element.component.ts
+++ data-binding/src/app/server-element/server-element.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ServerElement } from './server-element.model';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
 })
 export class ServerElementComponent {
-
+  @Input() element: ServerElement = {} as ServerElement;
 }
