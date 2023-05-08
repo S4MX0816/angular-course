0ec0a0dc1f996d4c5e846602bcc6ec7845ad6681
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-08 20:15:41 +0530

    creating an accounts service

--- services/src/app/new-account/new-account.component.ts
+++ services/src/app/new-account/new-account.component.ts
@@ -1,25 +1,25 @@
-import { Component, EventEmitter, Output, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
 import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService],
+  providers: [LoggingService, AccountsService],
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
   private loggingService: LoggingService;
+  private accountsService: AccountsService;
 
   constructor() {
     this.loggingService = inject(LoggingService);
+    this.accountsService = inject(AccountsService);
   }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus,
-    });
+    this.accountsService.addAccount(accountName, accountStatus);
     this.loggingService.logStatusChanged(accountStatus);
   }
 }
