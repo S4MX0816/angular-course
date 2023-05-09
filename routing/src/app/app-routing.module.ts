ecce47c10f96310e687546073fa28854cca696fa
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-10 01:17:50 +0530

    adding auth guard

--- routing/src/app/app-routing.module.ts
+++ routing/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { UserComponent } from './users/user/user.component';
 import { ServersComponent } from './servers/servers.component';
 import { UsersComponent } from './users/users.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AuthGuard } from './auth-guard.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -18,6 +19,7 @@ const appRoutes: Routes = [
   },
   {
     path: 'servers',
+    canActivate: [AuthGuard],
     component: ServersComponent,
     children: [
       { path: ':id', component: ServerComponent },
---
+++ routing/src/app/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  loggedIn = false;
+
+  isAuthenticated() {
+    const promise = new Promise<boolean>((resolve) => {
+      setTimeout(() => {
+        resolve(this.loggedIn);
+      }, 800);
+    });
+
+    return promise;
+  }
+
+  login() {
+    this.loggedIn = true;
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
+}
