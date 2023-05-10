b32e6e202e400295bf4ca28cf82797163069a8c9
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-10 23:51:38 +0530

    resolving dynamic data with resolve

--- routing/src/app/servers/server/server.component.ts
+++ routing/src/app/servers/server/server.component.ts
@@ -18,12 +18,14 @@ export class ServerComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    const id = +this.route.snapshot.params['id'];
-    this.server = this.serversService.getServer(id);
-
-    this.route.params.subscribe((params) => {
-      this.server = this.serversService.getServer(+params['id']);
+    this.route.data.subscribe((data) => {
+      this.server = data['server'];
     });
+    // const id = +this.route.snapshot.params['id'];
+    // this.server = this.serversService.getServer(id);
+    // this.route.params.subscribe((params) => {
+    //   this.server = this.serversService.getServer(+params['id']);
+    // });
   }
 
   onEdit() {
