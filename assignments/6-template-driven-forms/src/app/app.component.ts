34716669225b3e6175d09d266b7e2a24f66b0688
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-13 00:45:33 +0530

    template driven forms assignment

--- assignments/6-template-driven-forms/src/app/app.component.ts
+++ assignments/6-template-driven-forms/src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = '6-template-driven-forms';
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  selectedSubscription = this.subscriptions[1];
+  res = {
+    email: '',
+    password: '',
+    subscription: '',
+  };
+  formSubmitted = false;
+
+  onSubmit(form: NgForm) {
+    this.formSubmitted = true;
+    this.res.email = form.value.mail;
+    this.res.password = form.value.password;
+    this.res.subscription = form.value.subscription;
+    form.reset();
+  }
 }
