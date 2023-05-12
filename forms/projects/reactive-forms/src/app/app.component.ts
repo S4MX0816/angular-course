4b2e959ba15c28f70b305a0ed78b1261e714b780
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-13 01:46:34 +0530

    declaring and using form arrays

--- forms/projects/reactive-forms/src/app/app.component.ts
+++ forms/projects/reactive-forms/src/app/app.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+
+type hobby = FormControl<string>;
+interface SignUpForm {
+  userData: FormGroup<{
+    username: FormControl<string>;
+    email: FormControl<string>;
+  }>;
+  gender: FormControl<string>;
+  hobbies: FormArray<hobby>;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,19 +18,25 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
-  signUpForm: FormGroup;
+  signUpForm: FormGroup<SignUpForm>;
 
   ngOnInit(): void {
-    this.signUpForm = new FormGroup({
+    this.signUpForm = new FormGroup<SignUpForm>({
       userData: new FormGroup({
         username: new FormControl(null, Validators.required),
         email: new FormControl(null, [Validators.required, Validators.email]),
       }),
       gender: new FormControl('male'),
+      hobbies: new FormArray([]),
     });
   }
 
   onSubmit(): void {
     console.log(this.signUpForm);
+  }
+
+  onAddHobby(): void {
+    const control: hobby = new FormControl(null, Validators.required);
+    this.signUpForm.controls.hobbies.push(control);
   }
 }
