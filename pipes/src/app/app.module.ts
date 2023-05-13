a6c20e700dc9a58e22b9221044e185cf02f640ff
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-13 22:25:51 +0530

    creating a filter pipe

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ShortenPipe } from './shorten.pipe';
+import { FormsModule } from '@angular/forms';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
-  declarations: [AppComponent, ShortenPipe],
-  imports: [BrowserModule],
+  declarations: [AppComponent, ShortenPipe, FilterPipe],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
