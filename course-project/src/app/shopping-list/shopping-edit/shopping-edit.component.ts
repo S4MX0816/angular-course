600aa40bc597accd9ad7f268d9489786edbd6fb6
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-13 15:32:43 +0530

    setting editing item in form

--- course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,6 @@
 import { Subscription } from 'rxjs';
 import { NgForm } from '@angular/forms';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
@@ -11,9 +11,11 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f', { static: true }) slForm: NgForm;
   subscription: Subscription;
   editMode = false;
   editedItemIndex: number;
+  editedItem: Ingredient;
 
   constructor(private slService: ShoppingListService) {}
 
@@ -21,6 +23,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.subscription = this.slService.startedEditing.subscribe((index) => {
       this.editedItemIndex = index;
       this.editMode = true;
+      this.editedItem = this.slService.getIngredient(this.editedItemIndex);
+      this.slForm.setValue({
+        name: this.editedItem.name,
+        amount: this.editedItem.amount,
+      });
     });
   }
 
