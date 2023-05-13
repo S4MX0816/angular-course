99a10db3158587f8a503526c1e9018e727e89502
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-13 22:49:49 +0530

    async pipes

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  appStatus = new Promise((resolve) => {
+    setTimeout(() => resolve('critical'), 2000);
+  });
   servers = [
     {
       instanceType: 'medium',
