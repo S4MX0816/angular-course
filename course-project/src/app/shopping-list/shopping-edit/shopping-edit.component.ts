834ccd65ea74a4a6f029da55840431d7175067e4
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-13 15:28:24 +0530

    editing functionality setup

--- course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,19 +1,36 @@
-import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { NgForm } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
-import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css'],
 })
-export class ShoppingEditComponent {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+
   constructor(private slService: ShoppingListService) {}
+
+  ngOnInit() {
+    this.subscription = this.slService.startedEditing.subscribe((index) => {
+      this.editedItemIndex = index;
+      this.editMode = true;
+    });
+  }
 
   onAddItem(form: NgForm) {
     const name = form.value.name;
     const amount = form.value.amount;
     this.slService.addIngredient(new Ingredient(name, amount));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
