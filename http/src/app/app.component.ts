97115d7cc34d60c7279bfc82538a2a69d4c958ce
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-14 18:19:58 +0530

    http app setup

---
+++ http/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    console.log(postData);
+  }
+
+  onFetchPosts() {
+    // Send Http request
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+}
