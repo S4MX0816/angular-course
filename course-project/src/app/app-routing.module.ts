b2298e2641f5a9a1592d1b1d45861d9aca565b9f
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-18 20:49:57 +0530

    creating an auth module

--- course-project/src/app/app-routing.module.ts
+++ course-project/src/app/app-routing.module.ts
@@ -2,14 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Route, RouterModule } from '@angular/router';
 
-import { AuthComponent } from './auth/auth.component';
-
 const appRoutes: Route[] = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  {
-    path: 'auth',
-    component: AuthComponent,
-  },
 ];
 
 @NgModule({
