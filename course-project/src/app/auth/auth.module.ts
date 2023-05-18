1e6a26180bcda7793cc4be642af9c9889f3d1af2
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-18 21:10:13 +0530

    lazy loading shopping list and auth module

--- course-project/src/app/auth/auth.module.ts
+++ course-project/src/app/auth/auth.module.ts
@@ -10,7 +10,7 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     FormsModule,
     SharedModule,
-    RouterModule.forChild([{ path: 'auth', component: AuthComponent }]),
+    RouterModule.forChild([{ path: '', component: AuthComponent }]),
   ],
 })
 export class AuthModule {}
