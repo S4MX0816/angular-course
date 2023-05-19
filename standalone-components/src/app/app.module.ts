1d2c91f7c6ac5f48817afedf11947bcb3580de2a
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-19 18:56:00 +0530

    standalone components app setup

---
+++ standalone-components/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DetailsComponent } from './welcome/details/details.component';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [AppComponent, WelcomeComponent, DetailsComponent],
+  imports: [BrowserModule, SharedModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
