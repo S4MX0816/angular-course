3aac19e05c9622c3b04cba446467a397fcfe72dc
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-19 19:20:46 +0530

    fixing routing in standalone app

--- standalone-components/src/main.ts
+++ standalone-components/src/main.ts
@@ -1,10 +1,13 @@
+import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
 // import { AnalyticsService } from './app/shared/analytics.service';
 
 bootstrapApplication(AppComponent, {
   providers: [
     // AnalyticsService
+    importProvidersFrom(AppRoutingModule),
   ],
 });
