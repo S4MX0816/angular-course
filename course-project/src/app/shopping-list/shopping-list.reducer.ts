c73bf42348b672bae19a23c74f62ed78bf85f3ef
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-20 00:44:29 +0530

    adding logic to reducer

--- course-project/src/app/shopping-list/shopping-list.reducer.ts
+++ course-project/src/app/shopping-list/shopping-list.reducer.ts
@@ -1,7 +1,17 @@
+import { Action } from '@ngrx/store';
 import { Ingredient } from '../shared/ingredient.model';
 
 const initialState = {
   ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)],
 };
 
-export function ShoppingListReducer(state = initialState, action) {}
+export function ShoppingListReducer(state = initialState, action: Action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT': {
+      return {
+        ...state,
+        ingredient: [...state.ingredients, action],
+      };
+    }
+  }
+}
