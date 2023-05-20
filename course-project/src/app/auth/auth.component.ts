d2f7b320728270a8ecedac583ef33d0eace36205
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-21 01:09:02 +0530

    login via ngrx effect

--- course-project/src/app/auth/auth.component.ts
+++ course-project/src/app/auth/auth.component.ts
@@ -11,6 +11,9 @@ import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import { AlertComponent } from '../shared/alert/alert.component';
 import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
 
 @Component({
   selector: 'app-auth',
@@ -29,7 +32,8 @@ export class AuthComponent implements OnDestroy {
   constructor(
     private authService: AuthService,
     private router: Router,
-    private componentFactoryResolver: ComponentFactoryResolver
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private store: Store<fromApp.AppState>
   ) {}
 
   onSwitchMode() {
@@ -47,7 +51,8 @@ export class AuthComponent implements OnDestroy {
 
     this.isLoading = true;
     if (this.isLoginMode) {
-      authObs = this.authService.login(email, password);
+      // authObs = this.authService.login(email, password);
+      this.store.dispatch(new AuthActions.LoginStart({ email, password }));
     } else {
       authObs = this.authService.signup(email, password);
     }
--- course-project/src/app/auth/store/auth.effects.ts
+++ course-project/src/app/auth/store/auth.effects.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 
 import * as AuthActions from './auth.actions';
 import { environment } from 'src/environments/environment';
+import { Injectable } from '@angular/core';
 
 export interface AuthResponseData {
   idToken: string;
@@ -14,28 +15,42 @@ export interface AuthResponseData {
   registered?: boolean;
 }
 
+@Injectable()
 export class AuthEffects {
-  authLogin = createEffect(() =>
-    this.actions$.pipe(
-      ofType(AuthActions.LOGIN_START),
-      switchMap((authData: AuthActions.LoginStart) => {
-        return this.http
-          .post<AuthResponseData>(
-            `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseApiKey}`,
-            {
-              email: authData.payload.email,
-              password: authData.payload.password,
-              returnSecureToken: true,
-            }
-          )
-          .pipe(
-            catchError(of()),
-            map((resData) => {
-              of();
-            })
-          );
-      })
-    )
+  authLogin$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.LOGIN_START),
+        switchMap((authData: AuthActions.LoginStart) => {
+          return this.http
+            .post<AuthResponseData>(
+              `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseApiKey}`,
+              {
+                email: authData.payload.email,
+                password: authData.payload.password,
+                returnSecureToken: true,
+              }
+            )
+            .pipe(
+              map((resData) => {
+                const expirationDate = new Date(
+                  new Date().getTime() + +resData.expiresIn * 1000
+                );
+                return of(
+                  new AuthActions.Login({
+                    email: resData.email,
+                    userId: resData.localId,
+                    token: resData.idToken,
+                    expirationDate,
+                  })
+                );
+              }),
+              catchError((error) => {
+                return of();
+              })
+            );
+        })
+      ) as any
   );
 
   constructor(private actions$: Actions, private http: HttpClient) {}
