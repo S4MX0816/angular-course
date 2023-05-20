7a332fa39a5aefb307c7cda604463fe173254fbb
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-21 00:43:21 +0530

    defining login action

--- course-project/src/app/auth/store/auth.effects.ts
+++ course-project/src/app/auth/store/auth.effects.ts
@@ -1,8 +1,42 @@
-import { Actions, ofType } from '@ngrx/effects';
+import { catchError, map, of, switchMap } from 'rxjs';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { HttpClient } from '@angular/common/http';
+
 import * as AuthActions from './auth.actions';
+import { environment } from 'src/environments/environment';
+
+export interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
 
 export class AuthEffects {
-  authLogin = this.actions$.pipe(ofType(AuthActions.LOGIN_START));
+  authLogin = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.LOGIN_START),
+      switchMap((authData: AuthActions.LoginStart) => {
+        return this.http
+          .post<AuthResponseData>(
+            `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseApiKey}`,
+            {
+              email: authData.payload.email,
+              password: authData.payload.password,
+              returnSecureToken: true,
+            }
+          )
+          .pipe(
+            catchError(of()),
+            map((resData) => {
+              of();
+            })
+          );
+      })
+    )
+  );
 
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions, private http: HttpClient) {}
 }
