ddca67b3e532d7dbe375333b2951e53e333b4c05
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-21 01:32:32 +0530

    finishing the login effect

--- course-project/src/app/auth/store/auth.effects.ts
+++ course-project/src/app/auth/store/auth.effects.ts
@@ -1,10 +1,11 @@
-import { catchError, map, of, switchMap } from 'rxjs';
+import { catchError, map, of, switchMap, tap } from 'rxjs';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { HttpClient } from '@angular/common/http';
 
 import * as AuthActions from './auth.actions';
 import { environment } from 'src/environments/environment';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 export interface AuthResponseData {
   idToken: string;
@@ -17,41 +18,71 @@ export interface AuthResponseData {
 
 @Injectable()
 export class AuthEffects {
-  authLogin$ = createEffect(
-    () =>
-      this.actions$.pipe(
-        ofType(AuthActions.LOGIN_START),
-        switchMap((authData: AuthActions.LoginStart) => {
-          return this.http
-            .post<AuthResponseData>(
-              `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseApiKey}`,
-              {
-                email: authData.payload.email,
-                password: authData.payload.password,
-                returnSecureToken: true,
+  authLogin$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.LOGIN_START),
+      switchMap((authData: AuthActions.LoginStart) => {
+        return this.http
+          .post<AuthResponseData>(
+            `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseApiKey}`,
+            {
+              email: authData.payload.email,
+              password: authData.payload.password,
+              returnSecureToken: true,
+            }
+          )
+          .pipe(
+            map((resData) => {
+              const expirationDate = new Date(
+                new Date().getTime() + +resData.expiresIn * 1000
+              );
+              return new AuthActions.Login({
+                email: resData.email,
+                userId: resData.localId,
+                token: resData.idToken,
+                expirationDate,
+              });
+            }),
+            catchError((errorResp) => {
+              let errorMessage = 'An unknown error occurred!';
+              if (!errorResp.error || !errorResp.error.error) {
+                of(new AuthActions.LoginFail(errorMessage));
               }
-            )
-            .pipe(
-              map((resData) => {
-                const expirationDate = new Date(
-                  new Date().getTime() + +resData.expiresIn * 1000
-                );
-                return of(
-                  new AuthActions.Login({
-                    email: resData.email,
-                    userId: resData.localId,
-                    token: resData.idToken,
-                    expirationDate,
-                  })
-                );
-              }),
-              catchError((error) => {
-                return of();
-              })
-            );
-        })
-      ) as any
+              switch (errorResp.error.error.message) {
+                case 'EMAIL_EXISTS': {
+                  errorMessage = 'This email exists already.';
+                  break;
+                }
+                case 'EMAIL_NOT_FOUND': {
+                  errorMessage = 'This email does not exist.';
+                  break;
+                }
+                case 'INVALID_PASSWORD': {
+                  errorMessage = 'This password is not correct.';
+                  break;
+                }
+              }
+              return of(new AuthActions.LoginFail(errorMessage));
+            })
+          );
+      })
+    )
   );
 
-  constructor(private actions$: Actions, private http: HttpClient) {}
+  authSuccess = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.LOGIN),
+        tap(() => {
+          this.router.navigate(['/']);
+        })
+      ),
+    { dispatch: false }
+  );
+
+  constructor(
+    private actions$: Actions,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 }
