197dddb83083a528e68a80b291bfd8af23e7c89c
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-26 00:23:58 +0530

    set up app server module and main server file

--- angular-universal-manual/src/app/app.module.ts
+++ angular-universal-manual/src/app/app.module.ts
@@ -1,16 +1,12 @@
-import { NgModule } from '@angular/core';
+import { APP_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [{ provide: APP_ID, useValue: 'manually-setup-ng-universal-app' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
