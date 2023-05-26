eb386026432b95cd3741847d54b8ff1197b2c6e7
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-26 21:51:13 +0530

    testing components and service

--- unit-test/src/app/user/user.component.spec.ts
+++ unit-test/src/app/user/user.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
+import { UserService } from './user.service';
 
 describe('UserComponent', () => {
   let component: UserComponent;
@@ -17,5 +18,26 @@ describe('UserComponent', () => {
 
   it('should create the app', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should use the user from the service', () => {
+    const userService = fixture.debugElement.injector.get(UserService);
+    expect(userService.user.name).toEqual(component.user.name);
+  });
+
+  it('should display the username if the user is logged in', () => {
+    component.isLoggedIn = true;
+    fixture.detectChanges();
+    const compiled: HTMLDivElement = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p').textContent).toContain(
+      component.user.name
+    );
+  });
+
+  it("shouldn't display the username if the user is not logged in", () => {
+    const compiled: HTMLDivElement = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p').textContent).not.toContain(
+      component.user.name
+    );
   });
 });
