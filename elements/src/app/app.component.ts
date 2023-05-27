ab030eed192ba757bd1620920d61f1efee457fe0
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-27 17:17:47 +0530

    using angular elements

--- elements/src/app/app.component.ts
+++ elements/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { AlertComponent } from './alert.component';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'elements';
+  content: SafeHtml;
+
+  constructor(injector: Injector, domSanitizer: DomSanitizer) {
+    const AlertElement = createCustomElement(AlertComponent, { injector });
+    customElements.define('my-alert', AlertElement);
+    setTimeout(() => {
+      this.content = domSanitizer.bypassSecurityTrustHtml(
+        '<my-alert message="Rendered dynamically"></my-alert>'
+      );
+    }, 1000);
+  }
 }
--- elements/src/app/app.module.ts
+++ elements/src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AlertComponent } from './alert.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, AlertComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
